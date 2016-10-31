17b317823b05506e49a97be92182d492e5c3724b
David Cespedes <ingdavidcespedes@gmail.com>
2016-10-31 16:20:11 -0500

    Adding Sesión details and Cell Scaling

--- Ultravisual/InspirationCell.swift
+++ Ultravisual/InspirationCell.swift
@@ -12,28 +12,40 @@ class InspirationCell: UICollectionViewCell {
   
   @IBOutlet private weak var imageView: UIImageView!
   @IBOutlet private weak var imageCoverView: UIView!
+  @IBOutlet private weak var titleLabel: UILabel!
+  @IBOutlet private weak var timeAndRoomLabel: UILabel!
+  @IBOutlet private weak var speakerLabel: UILabel!
   
   var inspiration: Inspiration? {
     didSet {
       if let inspiration = inspiration {
         imageView.image = inspiration.backgroundImage
+        titleLabel.text = inspiration.title
+        timeAndRoomLabel.text = inspiration.roomAndTime
+        speakerLabel.text = inspiration.speaker
       }
     }
   }
-    override func applyLayoutAttributes(layoutAttributes: UICollectionViewLayoutAttributes) {
-        super.applyLayoutAttributes(layoutAttributes)
-        
-        // 1
-        let standardHeight = UltravisualLayoutConstants.Cell.standardHeight
-        let featuredHeight = UltravisualLayoutConstants.Cell.featuredHeight
-        
-        // 2
-        let delta = 1 - ((featuredHeight - CGRectGetHeight(frame)) / (featuredHeight - standardHeight))
-        
-        // 3
-        let minAlpha: CGFloat = 0.3
-        let maxAlpha: CGFloat = 0.75
-        imageCoverView.alpha = maxAlpha - (delta * (maxAlpha - minAlpha))
-    }
+  override func applyLayoutAttributes(layoutAttributes: UICollectionViewLayoutAttributes) {
+    super.applyLayoutAttributes(layoutAttributes)
+    
+    // 1
+    let standardHeight = UltravisualLayoutConstants.Cell.standardHeight
+    let featuredHeight = UltravisualLayoutConstants.Cell.featuredHeight
+    
+    // 2
+    let delta = 1 - ((featuredHeight - CGRectGetHeight(frame)) / (featuredHeight - standardHeight))
+    
+    // 3
+    let minAlpha: CGFloat = 0.3
+    let maxAlpha: CGFloat = 0.75
+    imageCoverView.alpha = maxAlpha - (delta * (maxAlpha - minAlpha))
+    
+    let scale = max(delta, 0.5)
+    titleLabel.transform = CGAffineTransformMakeScale(scale, scale)
+    
+    timeAndRoomLabel.alpha = delta
+    speakerLabel.alpha = delta
+  }
   
 }
